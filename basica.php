6b7b8bfa426ee87d99e676c452dcd87c659ca8df
anaescarcena <anaescalvarez@gmail.com>
2026-03-02 16:30:06 +0100

    Conflicts

--- basica.php
+++ basica.php
@@ -1,14 +1,8 @@
 <?php 
-// 1. Configuraciones siempre al principio
-define("pagina", "basica");
-$tituloprov = "Título básico";
-define("tituloprov", "Título específico de una web básica");
-
-// 2. Carga de cabecera
 include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
 ?>
 
-<p class="ctarepaso">JS para web básica</p>
+<p class="ctarepaso">Esto es un botón</p>
 <button type="button"
     onclick='document.getElementsByClassName("ctarepaso")[0].innerHTML = "Esto es un botón";'>
 
@@ -18,27 +12,52 @@ include $_SERVER['DOCUMENT_ROOT'].'/assets/header.php';
     Púlsame
 </button>
 
-<?php reutilizable1(); ?>
+            <?php reutilizable1();?>
 
-<h2 id="tipopantalla"></h2>
-<h3 id="estacion"></h3>
+<h2 id= "tipopantalla"></h2>
+<h3 id= "estacion"></h3>
 
 <script>
-// Lógica de detección de pantalla
 let anchoPantalla = window.innerWidth;
-let mensaje = (anchoPantalla > 1300) ? "Monitor" : (anchoPantalla > 700 ? "Portátil" : "Móvil");
-document.getElementById("tipopantalla").innerHTML = "Me estás viendo desde el " + mensaje;
+let mensaje;
 
-// Lógica de estación
+if (anchoPantalla > 1300) {
+    mensaje = "Me estás viendo desde el monitor";
+}
+else if (anchoPantalla <1300 && anchoPantalla > 700) {
+    mensaje = "Me estás viendo desde el portatil";
+}
+else {
+    mensaje = "Me estás viendo desde el móvil";
+}
+
+document.getElementById("tipopantalla").innerHTML = mensaje;
+
 let mes = new Date().getMonth();
-let periodo = ([6, 7].includes(mes)) ? "¡Sí! Es verano" : "No es verano actualmente";
+let periodo;
+
+switch (mes) {
+    case 0:
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 8:
+    case 9:
+    case 10:
+    case 11:
+        periodo = "No es verano actualmente";
+        break;
+    default:
+        periodo = "¡Sí! Es verano actualmente";
+}
+
 document.getElementById("estacion").innerHTML = periodo;
+
 </script>
 
-<script src="/scripts/pruebas.js"></script>
-
 <?php 
-// 4. El cierre siempre al final de TODO
 include $_SERVER['DOCUMENT_ROOT'].'/assets/footer.php';
 ?>
-<script src="/scripts/pruebas.js"></script>
+<!-- script src="/scripts/pruebas.js"></-script> (tiene un error y hace que se rompa todo. no recuerdo qué hace) -->

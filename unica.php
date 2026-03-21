3fbd72bd7882e3e36ba7f0358401052ab0b44d8b
anaescarcena <anaescalvarez@gmail.com>
2026-03-21 19:31:31 +0100

    Creo metaetiquetas básicas
    metaedescription y metatitle

---
+++ unica.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="es"> 
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta charset="UTF-8">
+        <title>Blog anita</title>        
+        <!-- <meta name="tittle" content="holiholiholi"> GRAN ERROR / CATETADA--->
+        <meta name="description" content="holiholiholi">
+
+    </head>
+
+    <body>
+        <header>
+            <nav></nav>
+        </header>
+                <h1>Página única</h1>
+
+
+        <footer>
+
+
+        </footer>
+
+    </body>
+    </html>

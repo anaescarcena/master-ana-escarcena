05b31a887920276ba3f052697e0853badd858505
anaescarcena <anaescalvarez@gmail.com>
2026-03-21 20:32:30 +0100

    meta name rating adult
    Recomendable para webs como airsoft, derivados marihuana... (no penalizacion pero lo tendrá en cuenta)

--- unica.php
+++ unica.php
@@ -12,10 +12,12 @@
         <!-- <meta name="robots" content="none"--->
         <!-- <meta name="robots" content="noindex, follow"--->
         <!-- <meta name="robots" content="noindex, nofollow""--->
-        <!-- <meta name="robots" content="all, noarchive" Que no se guarde en caché  --->
+        <!-- <meta name="robots" content="all, noarchive" Que no se guarde en caché  //  El atributo noarchive ha sido depreciado ya
+        que se eliminó la opción de "caché" de Google. Entonces está en desuso.--->
         <!-- <meta name="robots" content="notranslate" No se ofrece la traduccion de esta pagina en los resultados de busqueda --->
         <!-- <meta name="robots" content="unavailable_after: 2033-09-27" Se puede indexar hasta esa fecha --->
         <!-- <meta name="bingbot" content="none" Bing no puede indexarla --->
+        <!-- <meta name="rating" content="adult" Recomendable para webs como airsoft, derivados marihuana... (no penalizacion pero lo tendrá en cuenta) --->
 
 
 

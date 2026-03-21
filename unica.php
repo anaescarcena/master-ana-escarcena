02cc5feaa0c43f80441c6849422035a65af9f14e
anaescarcena <anaescalvarez@gmail.com>
2026-03-21 20:26:43 +0100

    Ejercicio metarobots
    (En el anterior commit puse metatitle MAL me corrijo, <title>)
    
    Creo un metarobots

--- unica.php
+++ unica.php
@@ -7,6 +7,22 @@
         <!-- <meta name="tittle" content="holiholiholi"> GRAN ERROR / CATETADA--->
         <meta name="description" content="holiholiholi">
 
+        <!-- <meta name="robots" content="index, follow"> Forma mas correcta para una página normal  --->
+        <!-- <meta name="robots" content="all"--->
+        <!-- <meta name="robots" content="none"--->
+        <!-- <meta name="robots" content="noindex, follow"--->
+        <!-- <meta name="robots" content="noindex, nofollow""--->
+        <!-- <meta name="robots" content="all, noarchive" Que no se guarde en caché  --->
+        <!-- <meta name="robots" content="notranslate" No se ofrece la traduccion de esta pagina en los resultados de busqueda --->
+        <!-- <meta name="robots" content="unavailable_after: 2033-09-27" Se puede indexar hasta esa fecha --->
+        <!-- <meta name="bingbot" content="none" Bing no puede indexarla --->
+
+
+
+        <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27"> ```
+
+
+
     </head>
 
     <body>

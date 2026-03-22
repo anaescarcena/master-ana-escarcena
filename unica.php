46c75fdeb7ddcb0e13a8bc18a7b5f55222b79f6d
anaescarcena <anaescalvarez@gmail.com>
2026-03-22 13:29:17 +0100

    OPEN GRAPHS Y TWITTER CARDS,

--- unica.php
+++ unica.php
@@ -3,29 +3,44 @@
     <head>
         <!-- <meta http-equiv="refresh" content="0; url=https://es.wikipedia.org/wiki/Estepona" /> Intentar ponerla lo más arriba posible--->
         <!--<meta http-equiv="refresh" content="15; url=https://es.wikipedia.org/wiki/Estepona" /> 15 Segundos para redirigir // Para redirecciones temporales un 2--->
-        <meta http-equiv="refresh" content="30; url=https://www.google.com/" />
+        <!--<meta http-equiv="refresh" content="30; url=https://www.google.com/" />--->
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <meta charset="UTF-8">
+        <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27"> ```
+
         <title>Blog anita</title>        
         <!-- <meta name="tittle" content="holiholiholi"> GRAN ERROR / CATETADA--->
         <meta name="description" content="holiholiholi">
 
         <!-- <meta name="robots" content="index, follow"> Forma mas correcta para una página normal  --->
-        <!-- <meta name="robots" content="all"--->
-        <!-- <meta name="robots" content="none"--->
-        <!-- <meta name="robots" content="noindex, follow"--->
-        <!-- <meta name="robots" content="noindex, nofollow""--->
-        <!-- <meta name="robots" content="all, noarchive" Que no se guarde en caché  //  El atributo noarchive ha sido depreciado ya
+        <!-- <meta name="robots" content="all"> --->
+        <!-- <meta name="robots" content="none">--->
+        <!-- <meta name="robots" content="noindex, follow"> --->
+        <!-- <meta name="robots" content="noindex, nofollow""> --->
+        <!-- <meta name="robots" content="all, noarchive" > Que no se guarde en caché  //  El atributo noarchive ha sido depreciado ya
         que se eliminó la opción de "caché" de Google. Entonces está en desuso.--->
-        <!-- <meta name="robots" content="notranslate" No se ofrece la traduccion de esta pagina en los resultados de busqueda --->
-        <!-- <meta name="robots" content="unavailable_after: 2033-09-27" Se puede indexar hasta esa fecha --->
-        <!-- <meta name="bingbot" content="none" Bing no puede indexarla --->
-        <!-- <meta name="rating" content="adult" Recomendable para webs como airsoft, derivados marihuana... (no penalizacion pero lo tendrá en cuenta) --->
+        <!-- <meta name="robots" content="notranslate">  No se ofrece la traduccion de esta pagina en los resultados de busqueda --->
+        <!-- <meta name="robots" content="unavailable_after: 2033-09-27">  Se puede indexar hasta esa fecha --->
+        <!-- <meta name="bingbot" content="none">  Bing no puede indexarla --->
+        <!-- <meta name="rating" content="adult">  Recomendable para webs como airsoft, derivados marihuana... (no penalizacion pero lo tendrá en cuenta) --->
+       <!-- <meta name="rating" content="indexifembedded"> --->
+        
+        <meta property="og:tittle" content="Unica | ejemplo. com " />
+        <meta property="og:description" content="sdg" />
+        <meta property="og:url" content="/master-ana-escarcena/unica" />
+        <meta property="og:image" content="/master-ana-escarcena/imagenes/carita.png" />
+        <meta property="og:image:secure_url" content="/master-ana-escarcena/imagenes/carita.png" />
+        <meta property="og:image:alt" content="Carita de ana" />
+        <meta property="og:type" content="website" />
 
+        <meta property="twitter:tittle" content="Unica | ejemplo. com " />
+        <meta property="twitter:description" content="sdg" />
+        <meta property="twitter:image" content="/master-ana-escarcena/imagenes/carita.png" />
+        <meta property="twitter:url" content="/master-ana-escarcena/unica" />
+        <meta name="twitter:card" content="summary_large_image" />
 
-
-        <meta name="googlebot" content="index, follow, max-snippet:25, noimageindex, unavailable_after: 2033-09-27"> ```
-
+        <meta name="twitter:site" content="@ANA_">
+        <meta name="twitter:creator" content="@ANA_">
 
 
     </head>

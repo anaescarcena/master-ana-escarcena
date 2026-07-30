92fbb57c2095b63c54f17fbd376b347de170f91b
anaescarcena <anaescalvarez@gmail.com>
2026-07-30 20:35:21 +0200

    Borro sitemap por defecto de Wordpress

--- wp-content/themes/asdrubal/functions.php
+++ wp-content/themes/asdrubal/functions.php
@@ -9,3 +9,11 @@ function tresposts (){
 add_shortcode('lastest_posts', 'tresposts');
 
 ?>
+
+<?php
+// Borrar Sitemap por defecto
+add_filter( 'wp_sitemaps_enabled', '__return_false' );
+if (has_action('init', 'wp_sitemaps_get_server')){
+remove_action('init', 'wp_sitemaps_get_server');
+}
+?>
